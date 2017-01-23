2fe1f2df47196ca2cd4956fd069e5cd60786e348
Enrico Gruner <evayly@linux-d5w2.suse>
2017-01-23 23:11:50 +0100

    lesson 01 and 02

---
+++ 02/02.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main() {
+    /**
+     * Datentypen: 
+     *   int        - Ganze Zahlen
+     *   float      - Komma Zahlen
+     *   char       - Einzelne Buchstaben oder Zeichen
+    **/
+
+    //int a = 3;
+    //float b = 3.34f;
+    //char c = 'A';
+
+    // printf() gibt einen Text aus.
+    //printf(" int a ist %d, float b ist %.2f, char c %c \n", a, b, c);
+
+    // Mit Variablen rechnen
+    int a = 3;
+    int b = 4;
+
+    printf( "Das Ergebnis aus %d + %d ist gleich %d \n", a,b, a+b );
+}
