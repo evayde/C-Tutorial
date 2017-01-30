1e91424067d2cef61ac67c93a25d6ef032935d96
Enrico Gruner <enrico.gruner@dev-creations.de>
2017-01-30 22:09:32 +0100

    03

---
+++ Basics/03/03_de.c
@@ -0,0 +1,22 @@
+/** Agenda
+  *  - Ein Gefühl für C bekommen
+  *  - C Strukturen kennenlernen (if)
+  *  - Besonderheiten kennenlernen (float)
+**/
+#include<stdio.h>
+
+void main() {
+  /** Einfache Genauigkeit gegen doppelte Genauigkeit
+  float a = 0.33;
+
+  if(a == 0.33f) {
+    printf("a ist 0.33\n");
+  }
+  else {
+    printf("a ist nicht 0.33\n");
+  }
+  **/
+
+  int a = 6;
+  printf("a/5 ist gleich %f \n", a/5);
+}
---
+++ Basics/03/03_en.c
@@ -0,0 +1,22 @@
+/** Agenda
+  *  - Get a feeling for C
+  *  - get to know a C Structure (if)
+  *  - get to know some specials (float)
+**/
+#include<stdio.h>
+
+void main() {
+    /** Single precision
+    float a = 0.33f;
+
+    if( a == 0.33f ) {
+      printf("a equals 0.33\n");
+    }
+    else {
+      printf("a is not equal to 0.33\n");
+    }
+    **/
+
+    float a = 6;
+    printf("a/5 equals %f \n", a/5);
+}
