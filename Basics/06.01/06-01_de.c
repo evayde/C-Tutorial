90d982c4cb03b1c7ebadbb3992ff3e28e4221420
Enrico Gruner <enrico.gruner@dev-creations.de>
2017-02-02 17:02:15 +0100

    06-01

---
+++ Basics/06.01/06-01_de.c
@@ -0,0 +1,10 @@
+/*
+ * Bastien Fournier
+ * Hi, I want to programming C with Visual Studio Code (Windows 10) but I don't know how to configure launch.json and task.json to run the debugger, have you an idea ? ty
+ */
+#include<stdio.h>
+
+void main() {
+  // STRG+SHIFT+P
+  printf("Hallo Bastien\n");
+}
